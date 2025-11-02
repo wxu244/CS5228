767477c8823f24d6b17bd790248a72dcd638b4a4
huiyvyao <2422314671@qq.com>
2025-11-02 17:24:34 +0800

    fix

--- project/src/catboost_model_all.py
+++ project/src/catboost_model_all.py
@@ -51,7 +51,7 @@ def main(data_dir: Path):
     print(f"Detected {len(cat_features)} categorical features: {cat_features}")
 
     # -------------------------------
-    # Train/Val split
+    # Train/Val split 
     # -------------------------------
     X_train, X_val, y_train, y_val = train_test_split(
         X, y, test_size=0.2, random_state=42
